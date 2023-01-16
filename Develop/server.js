6874dedb822ea2f077128654708d008b2494a4cb
“Nayonna <Nayonna Purnell>
2023-01-15 22:12:30 -0500

    Added the routes to the serverJS file

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
 
+const Product = require('./models/Product');
+const Category = require('./models/Category');
+const ProductTag = require('./models/ProductTag');
+const Tag = require('./models/Tag');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
