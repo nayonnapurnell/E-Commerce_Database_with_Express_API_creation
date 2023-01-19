06fea5d7081d8aa92afd626b9fe41957df5a7c5c
“Nayonna <Nayonna Purnell>
2023-01-18 21:05:19 -0500

    Added the sequelize models to the index.js file

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Products.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Categories.hasMany(Products, {
+  foreignKey: 'product_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Products.belongsToMany(Tags, {
+  foreignKey: 'product_tag_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tags.belongsToMany(Products, {
+  foreignKey: 'product_tag_id'
+});
 
 module.exports = {
   Product,
